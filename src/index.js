3780f803f1b0d28c570cf84ee010ec2a81db1f19
Dilara Katuk <dilarakatuk@gmail.com>
2022-06-05 12:56:32 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-const init = () => {
-  
+const init = (event) => {
+  event.preventDefault();
+  const form = document.querySelector('form') 
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = document.querySelector('#searchByID')
+    const id = input.value;
+    const url = `http://localhost:3000/movies/${id}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(json=> renderDetails(json) )
+  })
+}
+
+function renderDetails(data){
+    const movieTitle = document.querySelector('section#movieDetails h4')
+    movieTitle.innerText = data.title;
+    const movieSummary = document.querySelector('section#movieDetails p')
+    movieSummary.innerText = data.summary;
 }
 
 document.addEventListener('DOMContentLoaded', init);
